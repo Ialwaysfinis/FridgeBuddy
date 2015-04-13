fb91822a71168e8803ee6982609dba6d2b32e2d3
gnikchance@yahoo.com <gnikchance@yahoo.com>
2015-04-13 17:51:11 -0400

    IngList Is uploaded with a few strings. Now I have to find out how to populate RecList based on selected checkboxes in IngList

--- app/src/main/java/com/example/administrator/helloworld/Search.java
+++ app/src/main/java/com/example/administrator/helloworld/Search.java
@@ -5,23 +5,89 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ListAdapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Search extends Activity {
+
+
+    protected Cursor cursor2;
+    protected SQLiteDatabase db;
+    protected ListView ingList;
+    protected ListView recView;
+
+
     /** Called when the activity is first created. */
 
-    protected Cursor cursor;
-    protected ListAdapter adapter;
-    protected SQLiteDatabase db;
-    protected ListView recipeList;
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        db = (new DatabaseHelper(this)).getWritableDatabase();
+        ingList = (ListView) findViewById(R.id.inglistView);
+        recView = (ListView) findViewById(R.id.reclistView);
+
+        //SETS UP STRING VALUES FOR INGREDIENT LIST
+        List<String> your_array_list = new ArrayList<String>();
+        your_array_list.add("Eggs");
+        your_array_list.add("Milk");
+        your_array_list.add("Bread");
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_multiple_choice,
+                your_array_list );
+        ingList.setAdapter(arrayAdapter);
+
+
+        //THIS SETS THE LIST ITEMS TO BE CHECKED/UNCHECKED
+        ingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // change the checkbox state
+                CheckedTextView checkedTextView = ((CheckedTextView)view);
+                checkedTextView.setChecked(!checkedTextView.isChecked());
+
+
+
+
+
+        }
+    });
+
+}
+                //Method GenerateRecipe calls for the listView "recView" to populate
+
+    public void populate()
+
+    {
+        String selectedFromList = (ingList.getItemAtPosition(position));
+
+        cursor2 = db.rawQuery("SELECT _id, Name, Ingredients, Recipe FROM recipe WHERE Name || ' ' || Ingredients LIKE ?",
+                new String[]{"%" + selectedFromList.getText().toString() + "%"});
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                (List<String>) cursor2);
+        recView.setAdapter(adapter);
+
     }
+
+    public void GenerateRecipes(View view)
+
+    {
+        populate();
+
+    }
+
+
 }
